cb14c4d06cf2ee1eecf43e238a163ee69491197e
kabenke <kabenke_nana@yahoo.fr>
2022-03-25 22:41:49 +0100

    Basic of Redirection on Symfony and Some SQL-Database migrations

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+
+
+
+
+
+/**
+ * @Route("/products",name="products_")
+ */
+class ProductController extends  AbstractController
+{
+    /**
+     * @return Response
+     * @Route("/home",name="home")
+     */
+    public function index()
+    {
+        return $this ->render('product.html.twig',[]);
+    }
+
+
+    /**
+     * @Route("/{id}", name="show", requirements={"id":"\d+"})
+     */
+    public function show(Product $product):Response
+    {
+        return new Response('displaying ' .$product->getName());
+    }
+
+    /**
+     * @return Response
+     * @Route("/inventory" , name="inventory")
+     */
+    public function inventory()
+    {
+        return new Response('This the product inventory');
+    }
+}
